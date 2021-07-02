95b5641bc408e5781c4b992b929f0fd454d3bad2
Julius Kreutz <jk@julius-kreutz.de>
2021-07-03 01:55:09 +0200

    Reallocated certificates

--- src/main.rs
+++ src/main.rs
@@ -54,8 +54,8 @@ fn load_ssl() -> ServerConfig {
     use std::{fs::File, io::BufReader};
 
     let mut config = ServerConfig::new(NoClientAuth::new());
-    let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
-    let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
+    let cert_file = &mut BufReader::new(File::open("keys/cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("keys/key.pem").unwrap());
     let cert_chain = certs(cert_file).unwrap();
     let mut keys = pkcs8_private_keys(key_file).unwrap();
     config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
